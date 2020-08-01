edfbb6b5c72fc26d75d3fb8c5a230f2638918cae
Dhea <dheaaqn@gmail.com>
2020-08-01 18:17:57 +0700

    add Task 2 & Task 3 Week 2 Lesson 3

--- Week 2/2.3/Task2.js
+++ Week 2/2.3/Task2.js
@@ -1,1 +1,12 @@
-console.log('soon:)')
+const reverseWord = (text) => {
+    let result = []
+    let toArray = text.split(" ")
+
+    for (let x = (toArray.length) - 1; x >= 0; x--) {
+        result.push(toArray[x])
+    }
+
+    console.log(result.join(" "))
+}
+
+reverseWord('hai halo hai')
---
+++ Week 2/2.3/Task3.js
@@ -0,0 +1,82 @@
+const ArkFood = (harga, voucher, jarak, pajak) => {
+    console.log(`Harga: ${harga}`)
+
+    if (!voucher) {
+        promo = 0
+        console.log(`Potongan: ${promo}`)
+    } else {
+        promo = voucher(harga)
+    }
+
+    let printPajak = countPajak(pajak, harga)
+    let printJarak = countJarak(jarak, harga)
+
+    let subtotal = (harga - promo) + printPajak + printJarak
+    console.log(`Subtotal: ${subtotal}`)
+}
+
+const ARKAFOOD5 = (harga) => {
+    let potongan
+    let minimalPembelian = 50000
+    let maximalPotongan = 50000
+
+    if (harga < minimalPembelian) {
+        console.log('Minimal pembelian Rp. 50.000')
+    } else {
+        potongan = 0.5 * harga
+        if (potongan < maximalPotongan) {
+            console.log(`Potongan: ${potongan}`)
+        } else {
+            potongan = 50000
+            console.log(`Potongan: ${potongan}`)
+        }
+    }
+    return potongan
+}
+
+const DITRAKTIRDEMY = (harga) => {
+    let potongan
+    let minimalPembelian = 25000
+    let maximalPotongan = 30000
+
+    if (harga < minimalPembelian) {
+        console.log('Minimal pembelian Rp. 25.000')
+    } else {
+        potongan = 0.6 * harga
+        if (potongan < maximalPotongan) {
+            console.log(`Potongan: ${potongan}`)
+        } else {
+            potongan = 30000
+            console.log(`Potongan: ${potongan}`)
+        }
+    }
+    return potongan
+}
+
+const countPajak = (pajak, harga) => {
+    let resPajak
+    if (pajak === true) {
+        resPajak = 0.05 * harga
+        console.log(`Pajak: ${resPajak}`)
+    } else {
+        resPajak = 0
+        console.log(`Pajak: ${resPajak}`)
+    }
+    return resPajak
+}
+
+const countJarak = (jarak) => {
+    let ongkir
+    if (jarak <= 2) {
+        ongkir = 5000
+    } else if (jarak > 2) {
+        ongkir = ((jarak - 2) * 3000) + 5000
+        console.log(`Biaya Pengantaran: ${ongkir}`)
+    } else {
+        ongkir = 0
+        console.log(`Biaya Pengantaran: ${ongkir}`)
+    }
+    return ongkir
+}
+
+ArkFood(50000, false, 5, true)
